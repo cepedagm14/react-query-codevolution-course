b6212511f3df8112a14844bf81d3025f607e3c13
cepedagm14 <cepedagm14@gmail.com>
2022-06-21 01:35:47 -0400

    fetching react-query

--- src/components/RQSuperHero.page.jsx
+++ src/components/RQSuperHero.page.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
 
 const RQSuperHeroPage = () => {
-  return (
-    <div>RQSuperHero.page</div>
-  )
-}
+  const { isLoading, data } = useQuery("super-heroes", fetchSuperHeroes);
 
-export default RQSuperHeroPage
+  if (isLoading) {
+    return <p>Cargando.....</p>;
+  }
+  return (
+    <>
+      <h2>Super Heroes Page</h2>
+      {data?.data.map((hero) => {
+        return <div key={hero.id}>{hero.name}</div>;
+      })}
+    </>
+  );
+};
+
+export default RQSuperHeroPage;
