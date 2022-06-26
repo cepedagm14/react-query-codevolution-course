61ccbddd6d304c27d162b455849587f8ed375c6d
cepedagm14 <cepedagm14@gmail.com>
2022-06-25 22:05:57 -0400

    create custom hook fetching data

--- src/components/RQSuperHero.page.jsx
+++ src/components/RQSuperHero.page.jsx
@@ -1,27 +1,18 @@
 import React from "react";
-import { useQuery } from "react-query";
-import axios from "axios";
-
-const fetchSuperHeroes = () => {
-  return axios.get("http://localhost:4000/superheroes");
-};
+import { useSuperHeroesData } from "../hooks/useSuperHroesData";
 
 const RQSuperHeroPage = () => {
-  const { isLoading, data, isError, error, isFetching } = useQuery(
-    "super-heroes",
-    fetchSuperHeroes,
-    {
-      // cacheTime: 5000, esta propiedad se usa para agregar un tiempo de cache de las query, por defecto son 5 minutos
-      // staleTime: 30000, es el tiempo de permanencia de datos antes de pasar a obsoletos, lo ideal es dejarlo por defecto puesto que su valor es cero
-      // refetchOnMount: tiene como valores true - false - always
 
-      // refetchOnWindowFocus:
-      // refetchInterval: 2000,
-      // refetchIntervalInBackground: true,
-    }
+  const onSuccess = (data) => {
+    console.log("success", data);
+  };
+
+  const onError = () => {};
+
+  const { isLoading, data, isError, error } = useSuperHeroesData(
+    onSuccess,
+    onError
   );
-
-  console.log({ isLoading, isFetching });
 
   if (isLoading) {
     return <p>Cargando.....</p>;
@@ -32,8 +23,8 @@ const RQSuperHeroPage = () => {
   return (
     <>
       <h2>Super Heroes Page</h2>
-      {data?.data.map((hero) => {
-        return <div key={hero.id}>{hero.name}</div>;
+      {data.map((hero, index) => {
+        return <div key={index}>{hero}</div>;
       })}
     </>
   );
